7d449b79bbc6e9f84d1a05cbd7e1085b093c2d7c
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-20 23:55:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require ('express')
+
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("");
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,(err)=>{
+    if(err)
+    console.error();
+    else
+    console.log(`running on ${port}` );
+});
