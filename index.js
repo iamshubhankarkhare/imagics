58ce09dd4f4546aa824da8e69aec1fe7ece2bf11
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-04-22 02:12:47 +0530

    ui updated

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/uploads', express.static(path.join(__dirname + '/uploads')));
+app.use(express.static('./views'));
 //routes
 app.get('/',(req,res)=>{
     res.render('home')
